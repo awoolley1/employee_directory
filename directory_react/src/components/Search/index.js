68c37a9597085dd5fab14d1cf305f895a139c91d
Local Al <alexander.s.woolley@gmail.com>
2021-03-28 19:28:03 -0600

    search component live

--- directory_react/src/components/Search/index.js
+++ directory_react/src/components/Search/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./style.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form'
 
 function Search() {
   return (
-    <h1>
-      <span>Search here</span>
-    </h1>
+    <Form>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Search</Form.Label>
+    <Form.Control type="email" placeholder="Enter employee name" />
+  </Form.Group>
+</Form>
   );
 }
 
