3cc497f40865ef658dc643bc024dc4eeb3a69c3f
Local Al <alexander.s.woolley@gmail.com>
2021-03-29 15:45:06 -0600

    movve td into map

--- directory_react/src/components/Grid/index.js
+++ directory_react/src/components/Grid/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import API from "../../api/randomUser.js";
 import "./style.css";
 import Table from 'react-bootstrap/Table'
+import Button from 'react-bootstrap/ToggleButton'
 
 function Grid() {
   const [data, setData] = useState([]);
@@ -17,33 +18,30 @@ function Grid() {
   }, []);
 
   return (
-    <div>
-       {data.map((user, index) => (
-        <div key={index}>
-          <Table striped bordered hover>
-  <thead>
-    <tr>
-      <th>Photo</th>
-      <th>First Name</th>
-      <th>Last Name</th>
-      <th>Email</th>
-      <th>Location</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td><img src={user.picture.thumbnail} alt="headshot"/></td>
-      <td>{user.name.first}</td>
-      <td>{user.name.last}</td>
-      <td>{user.email}</td>
-      <td>{user.location.city}</td>
-    </tr>
-  </tbody>
+    <Table striped bordered hover variant="dark">
+    <thead>
+<tr>
+ <th class="row1">Photo</th> 
+ <th class="row1">First Name</th>
+ <th class="row1">Last Name</th>
+ <th class="row1">Email</th>
+ <th class="row1"><Button>Location</Button></th>
+</tr>
+</thead><tbody>
+
+{data.map((user, index) => (
+  <tr key={index}>
+  <td><img src={user.picture.thumbnail} alt="headshot"/></td>
+ <td>{user.name.first}</td>
+ <td>{user.name.last}</td>
+ <td>{user.email}</td>
+ <td>{user.location.city}</td>
+ </tr>
+ )
+)}
+</tbody>
 </Table>
-        </div>
-      ))}
-    </div>
-  );
+);
 }
 
 export default Grid;
