bb2842e6e20a65080a264b7eca4374952db3d7bc
Local Al <alexander.s.woolley@gmail.com>
2021-03-28 23:55:58 -0600

    moved API from container to Grid component

--- directory_react/src/components/Grid/index.js
+++ directory_react/src/components/Grid/index.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import API from "../../api/randomUser.js";
 import "./style.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Grid() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    API.getUser()
+      .then((res) => {
+        console.log(res);
+        setData(res.data.results);
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <h1>
-      <span>Grid here</span>
-    </h1>
+    <div>
+       {data.map((user, index) => (
+        <div key={index}>
+          <h3>{user.name.first}</h3>
+        </div>
+      ))}
+    </div>
   );
 }
 
--- directory_react/src/containers/directory.js
+++ directory_react/src/containers/directory.js
@@ -1,48 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React from "react";
 import Header from "../components/Header";
 import Search from "../components/Search";
 import Grid from "../components/Grid";
-import API from "../api/randomUser.js";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Directory() {
-  const [data, setData] = useState([]);
+  return (
+    <div>
+      <Header></Header>
 
-  useEffect(() => {
-  
+      <Search></Search>
+      
+      <Grid></Grid>
+    </div>
+  );
+}
 
-    API.getUser()
-      .then(res => {
-    console.log(res)
-       setData(res.data.results)
-       console.log(data)
-
-      })
-      .catch(err =>console.log(err));
-  }, []);
-
-    return (
-      <div>
-        <Header >
-          <h1>Header here</h1>
-        </Header>
-
-        <Search >
-          <h1>Search here</h1>
-        </Search>
-
-
-        {data.map((user, index) => (
-          <div key={index}>
-            <h3>{user.name.first}</h3>
-         
-          </div>
-        ))}
-        <Grid>
-             
-        </Grid>
-      </div>
-    );
-  }
-  
-  export default Directory;
+export default Directory;
